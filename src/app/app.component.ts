72517499395f9a91977414c37ff3a9c107f04948
chenying <chenying@uniontech.com>
2020-05-15 16:02:38 +0800

    feat: 添加国际化配置

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angulardemo';
+  language=true; //true 中文  false 英文
+
+  constructor( //国际化
+    private translate: TranslateService
+  ){
+    this.translate.use("zh_cn");
+  }
+
+  changeLan(){
+    console.log(this.language);
+    if(this.language){
+      this.translate.use("en_us");
+    }else{
+      this.translate.use("zh_cn");
+    }
+    this.language=!this.language;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GlobalManageComponent } from './global-manage/global-manage.component';
 import { ForumtypeComponent } from './forumtype/forumtype.component';
+// 国际化引入模块
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [
@@ -14,9 +21,59 @@ import { ForumtypeComponent } from './forumtype/forumtype.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    TranslateModule.forRoot({ //国际化
+      loader: {
+          provide: TranslateLoader,
+          useFactory: createTranslateLoader,
+          deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent] //主视图
 })
 export class AppModule { }
+
+
+// import { BrowserModule } from '@angular/platform-browser';
+// import { NgModule } from '@angular/core';
+// import { AppRoutingModule } from './app-routing.module';
+// import { FormsModule } from '@angular/forms';
+// // import { Http } from '@angular/common/http';
+// import {
+//   TranslateModule,
+//   TranslateLoader,
+//   TranslateService
+// } from '@ngx-translate/core';
+// import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+// import { AppComponent } from './app.component';
+// import {HttpClient, HttpClientModule} from '@angular/common/http';
+// export function createTranslateLoader(http: HttpClient) {
+//   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+// }
+// import { GlobalManageComponent } from './global-manage/global-manage.component';
+// import { ForumtypeComponent } from './forumtype/forumtype.component';
+// @NgModule({
+//   declarations: [
+//     AppComponent,
+//         GlobalManageComponent,
+//     ForumtypeComponent,
+//   ],
+//   imports: [
+//     BrowserModule,
+//     HttpClientModule,
+//     AppRoutingModule,
+//     TranslateModule.forRoot({
+//       loader: {
+//           provide: TranslateLoader,
+//           useFactory: createTranslateLoader,
+//           deps: [HttpClient]
+//       }
+//     })
+//   ],
+//   providers: [],
+//   bootstrap: [AppComponent]
+// })
+// export class AppModule { }
